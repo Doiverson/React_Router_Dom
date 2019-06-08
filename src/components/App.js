0b30821d8fe4b929495b60a47651778a6a96ec9b
Shosuke Doi <doiverson.canada@gmail.com>
2019-06-08 16:09:13 -0700

    update

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {BrowserRouter, Route, Link} from "react-router-dom"
+
+
+const PageOne = () => {
+    return (
+        <div>
+            <Link to="/pagetwo"></Link>
+        </div>
+    )
+}
+
+const PageTwo = () => {
+    return <div>Page Two</div>
+}
+
+const App = () => {
+
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne}/>
+                    <Route path="/pagetwo" component={PageTwo}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App
