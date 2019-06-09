af876a569e1b9eb9dd6d14a9f82867db8665bba9
Shosuke Doi <doiverson.canada@gmail.com>
2019-06-08 17:28:21 -0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import {BrowserRouter, Route, Link} from "react-router-dom"
+import {BrowserRouter, Route} from "react-router-dom"
+
+import StreamList from './streams/StreamList'
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamShow from './streams/StreamShow'
+import StreamDelete from './streams/StreamDelete'
+import Header from './Header'
 
 
-const PageOne = () => {
-    return (
-        <div>
-            <Link to="/pagetwo"></Link>
-        </div>
-    )
-}
-
-const PageTwo = () => {
-    return <div>Page Two</div>
-}
 
 const App = () => {
 
@@ -20,8 +16,13 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne}/>
-                    <Route path="/pagetwo" component={PageTwo}/>
+                    <Header/>
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/new" exact component={StreamCreate}/>
+                    <Route path="/streams/edit" exact component={StreamEdit}/>
+                    <Route path="/streams/delete" exact component={StreamDelete}/>
+                    <Route path="/streams/show" exact component={StreamShow}/>
+
                 </div>
             </BrowserRouter>
         </div>
