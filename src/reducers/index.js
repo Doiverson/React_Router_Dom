01557f1a8502c0e93e6048a0d5f922303f404abf
Shosuke Doi <doiverson.canada@gmail.com>
2019-09-07 15:51:04 -0700

    update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
-// import { reducer as formReducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 import authReducers from './authReducer';
 
 export default combineReducers({
    auth: authReducers,
+   form: formReducer
 });
