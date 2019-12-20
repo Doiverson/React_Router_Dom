58ff5a49ef6860e2d2522be91277f36c0b3dd014
Shosuke Doi <doiverson.canada@gmail.com>
2019-12-20 15:42:08 -0800

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -43,7 +43,7 @@ export const fetchStream = (id) => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.put(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch({ type: EDIT_STREAM, payload: response.data })
 };
@@ -52,6 +52,7 @@ export const deleteStream = (id, formValues) => async dispatch => {
     await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id })
+    history.push('/');
 };
 
 
